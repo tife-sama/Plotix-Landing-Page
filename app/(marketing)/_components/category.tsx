697d51d5fecb02493a5b30db9c20176ae5ffdff8
tife-sama <127571148+tife-sama@users.noreply.github.com>
2023-12-06 16:50:38 +0000

    Update category.tsx

--- app/(marketing)/_components/category.tsx
+++ app/(marketing)/_components/category.tsx
@@ -83,7 +83,7 @@ export default function Category() {
         </div>
         <ul
           role="list"
-          className="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+          className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
         >
           {category.map((categoryItem) => (
             <li key={categoryItem.name} className="relative">
